3b2b3c942b59fd0f84d3e5b48479d9fb46e2c369
bi-nho <131626706+bi-nho@users.noreply.github.com>
2024-05-17 23:34:25 -0300

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import AuthNavbar from '../../components/AuthNavbar';
+import GitHub from '../../components/img/GitHub.png'
 
 const About = () => {
   return (
     <div>
       <AuthNavbar />
       <p>Sobre Nós</p>
-      </div>
+      <a href="https://github.com/RuanAzevedoLira02/ATV02-REACT" target="_blank" rel="noopener noreferrer">
+        <img src={GitHub} alt="Github" />
+      </a>
+    </div>
   );
 };
 
