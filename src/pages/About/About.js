0c431d40e66ca159c5293855050fb5332a9fd10a
bi-nho <131626706+bi-nho@users.noreply.github.com>
2024-05-17 23:46:23 -0300

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
     <div>
       <AuthNavbar />
       <p>Sobre Nós</p>
-      <a href="https://github.com/RuanAzevedoLira02/ATV02-REACT" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/Andrey-Ranielly/Central-Sangue" target="_blank" rel="noopener noreferrer">
         <img src={GitHub} alt="Github" />
       </a>
     </div>
